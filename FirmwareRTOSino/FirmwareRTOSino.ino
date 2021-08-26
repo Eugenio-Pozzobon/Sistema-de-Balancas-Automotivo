698c40cab5dc1f1a0e1b4f3a8d31dafdab99c56c
zatgirl <mauren.d'avila@ecomp.ufsm.br>
2021-08-26 02:47:14 -0300

    Update FirmwareRTOSino.ino

--- FirmwareRTOSino/FirmwareRTOSino.ino
+++ FirmwareRTOSino/FirmwareRTOSino.ino
@@ -50,7 +50,7 @@ double td = 0;
 
 // define two tasks for Blink & AnalogRead
 void TaskBlink( void *pvParameters ); 
-void TaskAnalogRead( void *pvParameters );
+void TaskUpdateLCD( void *pvParameters );
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -72,7 +72,7 @@ void setup() {
     ,  NULL );
 
   xTaskCreate(
-    TaskAnalogRead
+    TaskUpdateLCD
     ,  "AnalogRead"
     ,  128  // Stack size
     ,  NULL
@@ -131,26 +131,44 @@ void TaskBlink(void *pvParameters)  // This is a task.
   }
 }
 
-void TaskAnalogRead(void *pvParameters)  // This is a task.
+void TaskUpdateLCD(void *pvParameters)  // This is a task.
 {
   (void) pvParameters;
   
-/*
-  AnalogReadSerial
-  Reads an analog input on pin 0, prints the result to the serial monitor.
-  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
-
-  This example code is in the public domain.
-*/
-
   for (;;)
   {
-    // read the input on analog pin 0:
-    int sensorValue = analogRead(A0);
-    // print out the value you read:
-    Serial.println(sensorValue);
-    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
+    if((millis() - latupdateTime) > 1000/UPDATE_LCD_HZ){
+        latupdateTime = millis();
+        state += (funcState? 1:0);
+        state = state > 3? 0: state;
+
+        if(tState){
+            tara();
+        }else{
+            switch (state) {
+                case 0:
+                    printScales();
+                    break;
+
+                    case 1:
+                        printLat();
+                        break;
+
+                        case 2:
+                            printLong();
+                            break;
+
+                            case 3:
+                                printTotal();
+                                break;
+
+                                default:
+                                    break;
+
+            }
+            funcState? delay(100):delay(1);
+        }
+    }
   }
 }
 
@@ -175,38 +193,7 @@ void TaskAnalogRead(void *pvParameters)  // This is a task.
 //    lcd.setCursor(0, 0);
 //    lcd.print(readTime_);
 //#else
-//    if((millis() - latupdateTime) > 1000/UPDATE_LCD_HZ){
-//        latupdateTime = millis();
-//        state += (funcState? 1:0);
-//        state = state > 3? 0: state;
-//
-//        if(tState){
-//            tara();
-//        }else{
-//            switch (state) {
-//                case 0:
-//                    printScales();
-//                    break;
-//
-//                    case 1:
-//                        printLat();
-//                        break;
-//
-//                        case 2:
-//                            printLong();
-//                            break;
-//
-//                            case 3:
-//                                printTotal();
-//                                break;
-//
-//                                default:
-//                                    break;
-//
-//            }
-//            funcState? delay(100):delay(1);
-//        }
-//    }
+//    
 //#endif
 //}
 //
@@ -216,46 +203,46 @@ void TaskAnalogRead(void *pvParameters)  // This is a task.
 //    calState = !digitalRead(CAL_PIN);
 //}
 //
-//void printScales(){
-//    lcd.clear();
-//    lcd.setCursor(0, 0); lcd.print((de));  //lcd.print(analogRead(FE_PORT)-512);//
-//    lcd.setCursor(9, 0); lcd.print((dd));  //lcd.print(analogRead(FD_PORT)-512);//
-//    lcd.setCursor(0, 1); lcd.print((te));  //lcd.print(analogRead(TE_PORT)-512);//
-//    lcd.setCursor(9, 1); lcd.print((td));  //lcd.print(analogRead(TD_PORT)-512);//
+void printScales(){
+    lcd.clear();
+    lcd.setCursor(0, 0); lcd.print((de));  //lcd.print(analogRead(FE_PORT)-512);//
+    lcd.setCursor(9, 0); lcd.print((dd));  //lcd.print(analogRead(FD_PORT)-512);//
+    lcd.setCursor(0, 1); lcd.print((te));  //lcd.print(analogRead(TE_PORT)-512);//
+    lcd.setCursor(9, 1); lcd.print((td));  //lcd.print(analogRead(TD_PORT)-512);//
+
+    lcd.setCursor(5, 0); lcd.print("kg");
+    lcd.setCursor(5, 1); lcd.print("kg");
+    lcd.setCursor(14, 0); lcd.print("kg");
+    lcd.setCursor(14, 1); lcd.print("kg");
+}
 //
-//    lcd.setCursor(5, 0); lcd.print("kg");
-//    lcd.setCursor(5, 1); lcd.print("kg");
-//    lcd.setCursor(14, 0); lcd.print("kg");
-//    lcd.setCursor(14, 1); lcd.print("kg");
-//}
-//
-//void printLong(){
-//    lcd.clear();
-//    lcd.setCursor(0, 0); lcd.print("Diant.: ");
-//    lcd.setCursor(0, 1); lcd.print("Tras.: ");
-//    lcd.setCursor(10, 0); lcd.print(int((de+dd)*100/total));
-//    lcd.setCursor(10, 1); lcd.print(int((te+td)*100/total));
-//    lcd.setCursor(15, 0); lcd.print("%");
-//    lcd.setCursor(15, 1); lcd.print("%");
-//}
-//
-//void printLat(){
-//    lcd.clear();
-//    lcd.setCursor(0, 0); lcd.print("Esq.: ");
-//    lcd.setCursor(11, 0); lcd.print("Dir.: ");
-//    lcd.setCursor(0, 1); lcd.print(int((de+te)*100/total));
-//    lcd.setCursor(11, 1); lcd.print(int((dd+td)*100/total));
-//    lcd.setCursor(4, 1); lcd.print("%");
-//    lcd.setCursor(15, 1); lcd.print("%");
-//}
-//
-//void printTotal(){
-//    lcd.clear();
-//    lcd.setCursor(4, 0); lcd.print("Total.: ");
-//    lcd.setCursor(5, 1); lcd.print(total);
-//    lcd.setCursor(10, 1); lcd.print("kg");
-//}
-//
+void printLong(){
+    lcd.clear();
+    lcd.setCursor(0, 0); lcd.print("Diant.: ");
+    lcd.setCursor(0, 1); lcd.print("Tras.: ");
+    lcd.setCursor(10, 0); lcd.print(int((de+dd)*100/total));
+    lcd.setCursor(10, 1); lcd.print(int((te+td)*100/total));
+    lcd.setCursor(15, 0); lcd.print("%");
+    lcd.setCursor(15, 1); lcd.print("%");
+}
+
+void printLat(){
+    lcd.clear();
+    lcd.setCursor(0, 0); lcd.print("Esq.: ");
+    lcd.setCursor(11, 0); lcd.print("Dir.: ");
+    lcd.setCursor(0, 1); lcd.print(int((de+te)*100/total));
+    lcd.setCursor(11, 1); lcd.print(int((dd+td)*100/total));
+    lcd.setCursor(4, 1); lcd.print("%");
+    lcd.setCursor(15, 1); lcd.print("%");
+}
+
+void printTotal(){
+    lcd.clear();
+    lcd.setCursor(4, 0); lcd.print("Total.: ");
+    lcd.setCursor(5, 1); lcd.print(total);
+    lcd.setCursor(10, 1); lcd.print("kg");
+}
+
 //void readData(){
 //    for(int i = 0; i < BUFFER_SIZE; i++){
 //        dataDe[i] = analogRead(FE_PORT)-511;
@@ -265,16 +252,16 @@ void TaskAnalogRead(void *pvParameters)  // This is a task.
 //    }
 //    processData();
 //}
-//
-//void tara(){
-//    lcd.clear();
-//    lcd.setCursor(0, 4); lcd.print("TARANDO");
-//    delay(100);
-//    taraDe += float(de/calibrationFactorDe);
-//    taraDd += float(dd/calibrationFactorDd);
-//    taraTe += float(te/calibrationFactorTe);
-//    taraTd += float(td/calibrationFactorTd);
-//}
+
+void tara(){
+    lcd.clear();
+    lcd.setCursor(0, 4); lcd.print("TARANDO");
+    delay(100);
+    taraDe += float(de/calibrationFactorDe);
+    taraDd += float(dd/calibrationFactorDd);
+    taraTe += float(te/calibrationFactorTe);
+    taraTd += float(td/calibrationFactorTd);
+}
 //
 //
 //void processData(){
