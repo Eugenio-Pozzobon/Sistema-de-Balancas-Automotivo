9c6f9c8afc7b4c654be089c94c0bb8b83e2efbe3
zatgirl <mauren.d'avila@ecomp.ufsm.br>
2021-08-26 00:31:26 -0300

    commit files

---
+++ Firmware RTOS/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/maure/Documents/GitHub/Sistema-de-Balancas-Automotivo/Firmware RTOS/src/main.cpp" "C:/Users/maure/Documents/GitHub/Sistema-de-Balancas-Automotivo/Firmware RTOS/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "C:/Users/maure/.platformio/packages/framework-arduino-avr/cores/arduino"
+  "C:/Users/maure/.platformio/packages/framework-arduino-avr/variants/standard"
+  "C:/Users/maure/.platformio/packages/framework-arduino-avr/libraries/EEPROM/src"
+  "C:/Users/maure/.platformio/packages/framework-arduino-avr/libraries/HID/src"
+  "C:/Users/maure/.platformio/packages/framework-arduino-avr/libraries/SPI/src"
+  "C:/Users/maure/.platformio/packages/framework-arduino-avr/libraries/SoftwareSerial/src"
+  "C:/Users/maure/.platformio/packages/framework-arduino-avr/libraries/Wire/src"
+  "C:/Users/maure/.platformio/packages/toolchain-atmelavr/lib/gcc/avr/7.3.0/include"
+  "C:/Users/maure/.platformio/packages/toolchain-atmelavr/lib/gcc/avr/7.3.0/include-fixed"
+  "C:/Users/maure/.platformio/packages/toolchain-atmelavr/avr/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
